b25b6c4ad6017a9e55b5b78e4e7b8f1886ac4940
JJ0ta19 <jjnieto-2022a@corhuila.edu.co>
2024-04-23 22:37:22 -0500

    Primer cambios

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,20 +2,46 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Carrito de compra</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Login</ion-title>
         </ion-toolbar>
       </ion-header>
+      <div id="login-container">
+        
+      
+        <div id="description">
+          <p>Quiz1-corte3</p>
+        </div>
+      
+        <div id="login-form">
+          <InputComponent id="Nombre" name="Nombre" label="Nombre: " />
+          <InputComponent type="date" id="Fecha" name="Fecha" label="Fecha de nacimiento: " />
+          <InputComponent type="nombre" id="Celular" name="Celular" label="Celular: " />
+          <InputComponent type="email" id="correo" name="correo" label="Correo: " />
+        </div>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p></p>
+        <div>
+          <ButtonComponent id="save" value="Agregar" fill="outline" color="success"/>  <p></p>
+          <ButtonComponent id="update" value="Modificar" fill="outline" color="warning"/>  <p></p>
+          <ButtonComponent id="danger" value="Eliminar" fill="outline" color="tertiary"/>  <p></p>
+          <ButtonComponent id="dark" value="Consultar" fill="outline" color="dark"/> 
+
+        </div>
+        <div>
+          <InputComponent type="password" id="personaId"  />
+        </div>
+
+        <!-- <div>
+          <ion-button fill="outline">Default</ion-button>
+          <ion-button :disabled="true" fill="outline">Disabled</ion-button>
+        </div> -->
       </div>
     </ion-content>
   </ion-page>
@@ -23,34 +49,36 @@
 
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import InputComponent from '@/components/InputComponent.vue';
 </script>
 
 <style scoped>
-#container {
+#login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#cart-image {
+  margin-bottom: 20px;
+  width: 100px;
+}
+
+#description {
+  margin-bottom: 20px;
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
+#description p {
   margin: 0;
+  padding: 10px; 
 }
 
-#container a {
-  text-decoration: none;
+#login-form {
+  max-width: 300px;
+  width: 100%;
 }
 </style>
